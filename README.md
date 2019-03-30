6e5bec2e9cbed6b648f50637c64d9e064a5b6f36
Shrey Saroch <shrey@adda247.com>
2019-03-31 03:25:57 +0530

    adding src files

---
+++ README.md
@@ -0,0 +1,10 @@
+A simple SPA Portfolio template for developer/designers built with React. Use it to showcase your work, testimonials and other information to clients.
+
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Change the values in ```src/resumeData.js``` to suit your use-case.
+4. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+5. Make required changes in ```src/resumeData.js``` to suit your needs.
+
---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+    "name": "Shrey Saroch",
+    "role": "Software Engineer",
+    "linkedinId":"https://www.linkedin.com/in/shrey-saroch-117a2766/",
+    "skypeid": "ishuzzz1",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/shrey-saroch-117a2766/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/shreysaroch",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/shreysaroch",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Working as a software engineer",
+    "address":"India",
+    "website":"http://shreysaroch.com shrey[dot]saroch[at]gmail[dot]com",
+    "education":[
+      {
+        "UniversityName":"National Institute of Technology, Hamirpur (H.P.)",
+        "specialization":"Computer Science & Engineering",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Snapdeal",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2016",
+      },
+      {
+        "CompanyName":"Adda247",
+        "specialization":"Sr. Software Engineer",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2018",
+      },
+        {
+            "CompanyName":"XSEED Education",
+            "specialization":"Sr. Frontend Engineer",
+            "MonthOfLeaving":"December",
+            "YearOfLeaving":"2018",
+        }
+    ],
+    "skillsDescription":"I usually like to experiment lot of things, some of are below",
+    "skills":[
+        {
+            "skillname":"AngularJS"
+        },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+        {
+            "skillname":"Javascript"
+        }
+    ],
+    "portfolio":[
+      {
+        "name":"Sherpalo",
+        "description":"Project Sherpalo was the codename for the revamp of Snapdeal Seller Dashboard.",
+        "imgurl":"images/portfolio/sd-logo.png"
+      },
+      {
+        "name":"Adda247 Store & Test taker platform",
+        "description":"This was the first web based project of the organization which was made single-handedly by me in a very short period of time",
+        "imgurl":"images/portfolio/adda-logo.png"
+      },
+      {
+        "name":"XSEED School WebApp",
+        "description":"This app had all the major features which are required for school-teacher-student-parent communication.",
+        "imgurl":"images/portfolio/xseed-image.png"
+      },
+      {
+        "name":"XSEED Test taker platform",
+        "description":"A platform where students can take assessments and the result could be seen in the XSEED School Webapp.",
+        "imgurl":"images/portfolio/xseed-image.png"
+      },
+        {
+            "name":"XSEED MAX Minicampus",
+            "description":"MAX is a weekly digital learning class in Math & Science based on the proven XSEED method. ",
+            "imgurl":"images/portfolio/xseed-image.png"
+        },
+        {
+            "name":"Adda247-Project Unity",
+            "description":"Project Unity is still operational, so I cannot share the details.",
+            "imgurl":"images/portfolio/adda-logo.png"
+        }
+    ],
+    "testimonials":[
+      {
+        "description":"If you want to get things done in right way Shrey is the person to go. Always structured in his thinking and approach he has the ability to dissect most difficult of problems. Given 18 hours to cut a tree Shrey is the kind of person who will sharpen his axe for 16 hours and this allows people around him to scale as they can then use the same axe to increase their productivity. It was great working with him and learned a lot from him directly as well as indirectly. He is definitely an asset to any team he joins.",
+        "name":"Gauraw Negi (Android Developer-MakeMyTrip)"
+      }
+    ]
+  };
+  
+  export default resumeData
